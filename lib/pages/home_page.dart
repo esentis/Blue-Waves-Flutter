ea217c9308f899a13f71bc576ba24ba49db99605
esentis <esentakos@yahoo.gr>
2020-09-29 15:25:58 +0300

    Dummy implementations

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,8 @@
+import 'package:blue_waves_flutter/models/RegisterMemberModel.dart';
 import 'package:blue_waves_flutter/pages/components/title.dart';
 import 'package:blue_waves_flutter/pages/components/tropical_island.dart';
 import 'package:flutter/material.dart';
-import 'package:lottie/lottie.dart';
+import 'package:blue_waves_flutter/connection.dart';
 
 import 'components/birds.dart';
 import 'components/sun.dart';
@@ -39,15 +40,45 @@ class _BlueWavesState extends State<BlueWaves> {
           left: MediaQuery.of(context).size.width / 4,
           child: BlueWavesTitle(),
         ),
+        // Positioned(
+        //   left: MediaQuery.of(context).size.width / 4,
+        //   top: MediaQuery.of(context).size.height / 10,
+        //   bottom: 0,
+        //   child: Lottie.network(
+        //     'https://assets5.lottiefiles.com/datafiles/EkoZpNsbNMU956i/data.json',
+        //     animate: true,
+        //     repeat: true,
+        //     width: 250,
+        //   ),
+        // ),
         Positioned(
-          left: MediaQuery.of(context).size.width / 4,
-          top: MediaQuery.of(context).size.height / 10,
           bottom: 0,
-          child: Lottie.network(
-            'https://assets5.lottiefiles.com/datafiles/EkoZpNsbNMU956i/data.json',
-            animate: true,
-            repeat: true,
-            width: 250,
+          child: FlatButton(
+            onPressed: () {
+              getBeaches();
+            },
+            child: const Text(
+              'GET ALL BEACHES',
+            ),
+          ),
+        ),
+        Positioned(
+          bottom: 0,
+          left: 200,
+          child: FlatButton(
+            onPressed: () {
+              var newMember = RegisterMemberModel(
+                userName: 'dummy',
+                email: 'dummy@dummy.gr',
+                password: 'Dummy1!!',
+                confirmPassword: 'Dummy1!!',
+              );
+
+              registerMember(newMember);
+            },
+            child: const Text(
+              'REGISTER MEMBER',
+            ),
           ),
         ),
       ],
