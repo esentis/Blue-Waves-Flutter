d711d30a8ec7f635d6ff421e3415cf1bf9741bce
esentis <esentakos@yahoo.gr>
2020-09-29 15:25:45 +0300

    Creates member register model

---
+++ lib/models/RegisterMemberModel.dart
@@ -0,0 +1,12 @@
+class RegisterMemberModel {
+  final String userName;
+  final String email;
+  final String password;
+  final String confirmPassword;
+  RegisterMemberModel({
+    this.confirmPassword,
+    this.email,
+    this.password,
+    this.userName,
+  });
+}
