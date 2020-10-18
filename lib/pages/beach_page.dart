151de60a834d2d19b61ea28ec82eb6c3e521a8eb
esentis <esentakos@yahoo.gr>
2020-10-19 01:36:02 +0300

    Styles up beach page

--- lib/pages/beach_page.dart
+++ lib/pages/beach_page.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
-
-import 'package:blue_waves_flutter/pages/components/animated_background/animated_background.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class BeachPage extends StatefulWidget {
@@ -34,7 +33,6 @@ class _BeachPageState extends State<BeachPage> {
       child: Scaffold(
         body: Stack(
           children: [
-            const AnimatedBackground(),
             CustomScrollView(
               slivers: [
                 SliverAppBar(
@@ -44,35 +42,94 @@ class _BeachPageState extends State<BeachPage> {
                   elevation: 40,
                   floating: true,
                   forceElevated: true,
-                  flexibleSpace: Image.network(
-                    widget.beach['images'][0],
-                    fit: BoxFit.cover,
+                  flexibleSpace: Hero(
+                    tag: widget.beach['images'][0],
+                    child: ClipRRect(
+                      borderRadius: const BorderRadius.only(
+                        bottomLeft: Radius.circular(20),
+                        bottomRight: Radius.circular(20),
+                      ),
+                      child: Image.network(
+                        widget.beach['images'][0],
+                        fit: BoxFit.cover,
+                      ),
+                    ),
                   ),
                 ),
                 SliverList(
                   delegate: SliverChildBuilderDelegate(
                     (context, index) {
-                      if (index == 44) {
+                      if (index == 0) {
+                        return Padding(
+                          padding: const EdgeInsets.all(12.0),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              boxShadow: [
+                                BoxShadow(
+                                  blurRadius: 2,
+                                  spreadRadius: 2,
+                                  color: Colors.black.withOpacity(0.2),
+                                ),
+                              ],
+                            ),
+                            child: Text(
+                              widget.beach['description'],
+                              style: GoogleFonts.adventPro(
+                                fontSize: 22,
+                              ),
+                            ),
+                          ),
+                        );
+                      } else if (index == 1) {
+                        return const SizedBox();
+                        // return Container(
+                        //   width: 150,
+                        //   height: 150,
+                        //   child: ListView.builder(
+                        //     itemBuilder: (context, index) {
+                        //       if (index == 0) {
+                        //         return const SizedBox();
+                        //       } else {
+                        //         return Padding(
+                        //           padding: const EdgeInsets.all(8.0),
+                        //           child: ClipRRect(
+                        //             borderRadius: BorderRadius.circular(20),
+                        //             child: BWavesImage(
+                        //                 url: widget.beach['images'][index]),
+                        //           ),
+                        //         );
+                        //       }
+                        //     },
+                        //     scrollDirection: Axis.horizontal,
+                        //     itemCount: widget.beach['images'].length,
+                        //   ),
+                        // );
+                      } else {
                         return Container(
                           width: MediaQuery.of(context).size.width,
                           height: MediaQuery.of(context).size.height / 2,
-                          child: GoogleMap(
-                            mapType: MapType.normal,
-                            markers: {beachMarker},
-                            zoomControlsEnabled: true,
-                            zoomGesturesEnabled: true,
-                            mapToolbarEnabled: true,
-                            myLocationButtonEnabled: false,
-                            initialCameraPosition: beachPlace,
-                            onMapCreated: (GoogleMapController controller) {
-                              _controller.complete(controller);
-                            },
+                          child: ClipRRect(
+                            borderRadius: const BorderRadius.only(
+                              topRight: Radius.circular(20),
+                              topLeft: Radius.circular(20),
+                            ),
+                            child: GoogleMap(
+                              mapType: MapType.normal,
+                              markers: {beachMarker},
+                              zoomControlsEnabled: true,
+                              zoomGesturesEnabled: true,
+                              mapToolbarEnabled: true,
+                              myLocationButtonEnabled: false,
+                              initialCameraPosition: beachPlace,
+                              onMapCreated: (GoogleMapController controller) {
+                                _controller.complete(controller);
+                              },
+                            ),
                           ),
                         );
                       }
-                      return Text('$index');
                     },
-                    childCount: 45,
+                    childCount: 3,
                   ),
                 ),
               ],
