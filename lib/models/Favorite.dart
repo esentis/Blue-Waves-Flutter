fd25ab164bc1a292a89c36145a93d8b0463d1c28
esentis <esentakos@yahoo.gr>
2020-10-27 00:21:24 +0200

    Adds beach name on favorites and ratings

--- lib/models/Favorite.dart
+++ lib/models/Favorite.dart
@@ -2,9 +2,11 @@ class Favorite {
   final String userId;
   final String beachId;
   final String id;
+  final String beachName;
   const Favorite({
     this.beachId,
     this.userId,
     this.id,
+    this.beachName,
   });
 }
--- lib/models/Rating.dart
+++ lib/models/Rating.dart
@@ -3,11 +3,13 @@ class Rating {
   final double rating;
   final String userID;
   final String username;
+  final String beachName;
 
   const Rating({
     this.beachId,
     this.rating,
     this.userID,
     this.username,
+    this.beachName,
   });
 }
