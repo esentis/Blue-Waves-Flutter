e4394bd945a6fc3aa7dcef9fb885e40b9b5c96ff
esentis <esentakos@yahoo.gr>
2020-11-05 21:41:58 +0200

    Edits rating model

--- lib/models/Rating.dart
+++ lib/models/Rating.dart
@@ -2,14 +2,10 @@ class Rating {
   final String beachId;
   final double rating;
   final String userUid;
-  final String username;
-  final String beachName;
 
   const Rating({
     this.beachId,
     this.rating,
     this.userUid,
-    this.username,
-    this.beachName,
   });
 }
