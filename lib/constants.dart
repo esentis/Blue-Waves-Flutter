9545b5cdc9c335b5d36de8d21f135b93539e5d76
George Leonidis <esentakos@yahoo.gr>
2021-06-20 23:38:22 +0300

    ✨ Creates constant style

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:logger/logger.dart';
 
 typedef Json = Map<String, dynamic>;
 var log = Logger();
+var kStyleDefault = GoogleFonts.adventPro(
+  fontSize: 20,
+  color: Colors.white,
+);
+
+var kStyleDefaultBold = GoogleFonts.adventPro(
+  fontSize: 20,
+  color: Colors.white,
+  fontWeight: FontWeight.bold,
+);
