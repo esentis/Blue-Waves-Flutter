660c6437178d3595f37cdf7e87f04646e9daaf45
George Leonidis <esentakos@yahoo.gr>
2021-08-04 23:03:35 +0300

    ✨ Adds google sign

--- lib/pages/register_login_page/login_page.dart
+++ lib/pages/register_login_page/login_page.dart
@@ -1,5 +1,7 @@
+import 'package:blue_waves/api/api_service.dart';
 import 'package:blue_waves/constants.dart';
 import 'package:blue_waves/generated/l10n.dart';
+import 'package:blue_waves/models/member.dart';
 import 'package:blue_waves/pages/components/loader.dart';
 import 'package:blue_waves/pages/components/snack_bar.dart';
 import 'package:blue_waves/states/loading_state.dart';
@@ -35,60 +37,121 @@ class _LoginPageState extends State<LoginPage> {
             const AnimatedBackground(
               showTitle: true,
             ),
-            Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  BWTextField(
-                    emailController: emailController,
-                    labelText: 'Email',
-                    type: TextInputType.emailAddress,
-                  ),
-                  BWPasswordField(
-                    emailController: passwordController,
-                    labelText: S.current.pass,
-                  ),
-                  TextButton(
-                    onPressed: () async {
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 10.h,
+                ),
+                BWTextField(
+                  emailController: emailController,
+                  labelText: 'Email',
+                  type: TextInputType.emailAddress,
+                ),
+                BWPasswordField(
+                  emailController: passwordController,
+                  labelText: S.current.pass,
+                ),
+                TextButton(
+                  onPressed: () async {
+                    loadingState.toggleLoading();
+
+                    try {
+                      await auth.signInWithEmailAndPassword(
+                          email: emailController.text,
+                          password: passwordController.text);
+
                       loadingState.toggleLoading();
-
-                      try {
-                        await auth.signInWithEmailAndPassword(
-                            email: emailController.text,
-                            password: passwordController.text);
-
-                        loadingState.toggleLoading();
-                        await Get.offAllNamed('/home');
-                      } on FirebaseAuthException catch (e) {
-                        loadingState.toggleLoading();
-                        showSnack(
-                          title: S.current.error,
-                          message: e.message!,
-                          firstColor: Colors.red,
-                          secondColor: Colors.red[800]!,
-                          duration: 2800,
-                        );
-                        log.e(e);
-                      }
-                    },
-                    child: Text(
-                      S.current.login,
-                      style: kStyleDefault.copyWith(
-                        fontSize: 25.sp,
-                      ),
+                      await Get.offAllNamed('/home');
+                    } on FirebaseAuthException catch (e) {
+                      loadingState.toggleLoading();
+                      showSnack(
+                        title: S.current.error,
+                        message: e.message!,
+                        firstColor: Colors.red,
+                        secondColor: Colors.red[800]!,
+                        duration: 2800,
+                      );
+                      log.e(e);
+                    }
+                  },
+                  child: Text(
+                    S.current.login,
+                    style: kStyleDefault.copyWith(
+                      fontSize: 25.sp,
                     ),
                   ),
-                  TextButton(
-                    onPressed: Get.back,
-                    child: Text(
-                      S.current.back,
-                      style: kStyleDefaultBold.copyWith(
-                        color: Colors.red[400]!.withOpacity(0.8),
+                ),
+                SizedBox(
+                  height: 10.h,
+                ),
+                Row(
+                  children: [
+                    Flexible(
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 20.0.w),
+                        child: Container(
+                          height: 1.h,
+                          color: Colors.white,
+                        ),
                       ),
                     ),
-                  )
-                ],
-              ),
+                    Text(
+                      S.current.or,
+                      style: kStyleDefault,
+                    ),
+                    Flexible(
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 20.0.w),
+                        child: Container(
+                          height: 1.h,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 10.h,
+                ),
+                GestureDetector(
+                  onTap: () async {
+                    UserCredential? user;
+                    try {
+                      user = await googleSign();
+                      log.wtf(user);
+                      if (user != null) {
+                        final isRegistered =
+                            await Api.instance.checkUser(user.user!.uid);
+                        if (!isRegistered) {
+                          log.i('Google user not registered.');
+                          await Api.instance.registerGoogleUser(
+                              id: user.user!.uid,
+                              displayName: user.user!.email!);
+                        }
+                      }
+                    } catch (e) {
+                      log.e(e);
+                    }
+                  },
+                  child: Image.asset(
+                    'assets/images/gsign.png',
+                    height: 50.h,
+                  ),
+                ),
+                SizedBox(
+                  height: 10.h,
+                ),
+                TextButton(
+                  onPressed: Get.back,
+                  child: Text(
+                    S.current.back,
+                    style: kStyleDefaultBold.copyWith(
+                      color: Colors.red[400]!.withOpacity(0.8),
+                    ),
+                  ),
+                )
+              ],
             ),
             // ignore: prefer_if_elements_to_conditional_expressions
             loadingState.isLoading! ? const Loader() : const SizedBox(),
