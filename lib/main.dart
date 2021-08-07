7f3a30e57a912da1f878c465f27d89bdb0ed69dc
George Leonidis <esentakos@yahoo.gr>
2021-08-07 16:25:27 +0300

    ➕ Adds connectivity dependency

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
+import 'package:blue_waves/constants.dart';
 import 'package:blue_waves/generated/l10n.dart';
 import 'package:blue_waves/pages/beach_page/beach_page.dart';
 import 'package:blue_waves/pages/register_login_page/login_page.dart';
 import 'package:blue_waves/pages/register_login_page/register_page.dart';
+
 import 'package:blue_waves/states/loading_state.dart';
 import 'package:blue_waves/pages/home_page.dart';
 import 'package:blue_waves/states/theme_state.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_analytics/observer.dart';
 import 'package:firebase_core/firebase_core.dart';
